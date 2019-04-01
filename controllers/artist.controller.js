f1e52ded76fefd60a3aabf6ce314bdc1a447c2c6
Carlos Díaz Girol <carlosdiazgirol@MacBook-Pro-de-Carlos-2.local>
2019-04-01 19:49:36 +0200

    fit: spotify data save mongo

--- controllers/artist.controller.js
+++ controllers/artist.controller.js
@@ -1,5 +1,5 @@
 const Artist = require('../models/artist.model');
-const spotifyApi = require('../services/spotify.service');
+const spotifyService = require('../services/spotify.service');
 
 const createError = require('http-errors');
 
@@ -11,46 +11,18 @@ module.exports.list = (req, res, next) => {
 
 module.exports.get = (req, res, next) => {
   Artist.findOne({name: req.params.name})
-    .then(async artist => {
+    .then(artist => {
       console.info('ARTIST => ', artist)
       if (!artist) {
         throw createError(404, 'Artist not found');
       }
-      // if (!artist.related && !artist.songs) {
-      try {
-        const { body } = await spotifyApi.searchArtists(artist.name, 'GB')
-        const { id } = 
-          body.artists.items.find(
-            item => item.name.toLowerCase() === artist.name.toLowerCase() && item.genres.includes('hip hop'))
-        console.info('UNIQUE => ', id)
-        const related = await spotifyApi.getArtistRelatedArtists(id)
-        console.info('RELATED => ', related)
-      } catch(error) {
-        console.info('CATCH ERROR => ', error)
-      }
-      // Promise.all([
-      //   spotifyApi.getArtistTopTracks(artist.name, 'GB'),
-      //   spotifyApi.getArtistRelatedArtists(artist.name)
-      // ]).then(([
-      //   topTracks,
-      //   relatedArtists
-      //   ]) => {
-      //     console.log('topTracks =>' + topTracks, 'relatedArtist' + relatedArtists)
-
-      //   })
-        // .catch(next)
-
-
-        // spotifyApi.searchArtists(artist.name)
-        // .then((data) => {
-        //   console.log(data.body.artists.items[0])
-        //   // Artist.findByIdAndUpdate(artist.id, 
-        //   //   { related: data.related,
-        //   //     songs: data.songs
-        //   //    }, {new: true}
-        //   // ).then(res.status(200).json(artist))
-        // })
-      }
-    )
-    .catch(next);
+      return spotifyService.getArtistReatedInfo(artist)
+        .then(([traks, related]) => {
+          return Artist.findOneAndUpdate(
+            { name: artist.name }, 
+            { $set: { related: related, songs: traks }},
+            { new: true }
+          ).then(artist => res.status(200).json(artist))
+        })
+  }).catch(next);
 }
--- services/spotify.service.js
+++ services/spotify.service.js
@@ -15,5 +15,37 @@ spotifyApi.clientCredentialsGrant()
   }, (err) => {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+module.exports.getArtistReatedInfo = (artist) => {
+  return spotifyApi.searchArtists(artist.name)
+    .then(res => {
+      const { id } = res.body.artists.items.find(
+        item => item.name.toLowerCase() === artist.name.toLowerCase() && item.genres.includes('hip hop'))
+        return Promise.all([
+          spotifyApi.getArtistTopTracks(id, 'GB'),
+          spotifyApi.getArtistRelatedArtists(id)
+        ])
+    }).then(([traksRes, relatedRes]) => {
+      console.log(relatedRes.body)
+      return Promise.resolve([
+        traksRes.body.tracks,
+        relatedRes.body.artists
+      ])
+    })
   
-module.exports = spotifyApi;  
+}
+
+
+/**
+const { body } = await spotifyApi.searchArtists(artist.name)
+const { id } = 
+  body.artists.items.find(
+    item => item.name.toLowerCase() === artist.name.toLowerCase() && item.genres.includes('hip hop'))
+console.info('UNIQUE => ', id)
+const related = await spotifyApi.getArtistRelatedArtists(id)
+console.info('RELATED => ', related)
+const topTracks = await spotifyApi.getArtistTopTracks(id, 'GB')
+console.info('TRACKS => ', topTracks.body.tracks)
+ */
+  
+module.exports.api = spotifyApi;  
